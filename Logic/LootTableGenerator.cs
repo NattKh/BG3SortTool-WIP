52cb8235d18d4a2727ae07b90ef6ff435aa9764c
NattKh <natthawut.khamnuadi@gmail.com>
2023-08-22 18:44:44 -0500

    adding more logic for directory entries

--- Logic/LootTableGenerator.cs
+++ Logic/LootTableGenerator.cs
@@ -50,9 +50,49 @@ public class LootTableGenerator
             string relativeFilePath = filePath.Replace(sourceDir, "").TrimStart('/');
             IEnumerable<XElement> nodes = XDocument.Load(filePath).Root!.XPathSelectElements("region[@id = 'Templates']/node/children/node[@id = 'GameObjects'][attribute[@id='Type' and @value='item']]");
 
+            //Populate _entries Dictionary Logic loop code
+
             foreach (XElement node in nodes)
             {
-                // ... (logic to extract attributes and populate _entries dictionary)
+                string? GetAttribute(string id, string name) => node.XPathSelectElement($"attribute[@id='{id}']")?.Attribute(name)?.Value;
+                string? GetAttributeValue(string id) => GetAttribute(id, "value");
+                string? GetAttributeHandle(string id) => GetAttribute(id, "handle");
+
+                string? mapKey = GetAttributeValue("MapKey");
+                string? name = GetAttributeValue("Name");
+
+                if (string.IsNullOrEmpty(mapKey) || string.IsNullOrEmpty(name))
+                {
+                    Console.WriteLine($"Error in {filePath}");
+                    continue;
+                }
+
+                string? parent = GetAttributeValue("ParentTemplateId");
+                string? displayName = GetAttributeHandle("DisplayName");
+                if (displayName != null) _localization.TryGetValue(displayName, out displayName);
+
+                string? technicalDescription = GetAttributeHandle("TechnicalDescription");
+                if (technicalDescription != null) _localization.TryGetValue(technicalDescription, out technicalDescription);
+
+                string? description = GetAttributeHandle("Description");
+                if (description != null) _localization.TryGetValue(description, out description);
+
+                ItemEntry entry = new(
+                    Name: name,
+                    Inheritance: null,
+                    MapKey: mapKey,
+                    Path: relativeFilePath,
+                    Data: new(
+                        ParentTemplateId: GetAttributeValue("ParentTemplateId"),
+                        VisualTemplateId: GetAttributeValue("VisualTemplate"),
+                        Stats: GetAttributeValue("Stats"),
+                        Icon: GetAttributeValue("Icon")),
+                    Localization: new(
+                        DisplayName: displayName,
+                        TechnicalDescription: technicalDescription,
+                        Description: description));
+
+                _entries[mapKey] = entry;
             }
         }
     }
@@ -79,6 +119,35 @@ public class LootTableGenerator
         );
 
         // ... (additional logic to generate armor and treasure table data and write to separate files)
+        IEnumerable<ItemEntry> armours = _entries.Values.Where(x => x.InheritsFrom("BASE_ARMOR"));
+        IEnumerable<ItemEntry> armoursWithStats = armours.Where(x => !string.IsNullOrWhiteSpace(x.Data.Stats));
+        IEnumerable<ItemEntry> armoursWithoutStats = armours.Except(armoursWithStats);
+
+        List<string> armourStatNames = armoursWithStats.Select(x => x.Data.Stats!).ToList();
+        List<string> generatedArmourTxt = new();
+
+        foreach (ItemEntry armour in armoursWithoutStats)
+        {
+            string statsName = $"LIA_GENERATED_{armour.Name}";
+            string? inheritedStats = armour.GetStats(_entries.Values);
+
+            if (!string.IsNullOrWhiteSpace(inheritedStats))
+            {
+                generatedArmourTxt.Add(
+                    $"new entry \"{statsName}\"\n" +
+                    $"type \"Armor\"\n" +
+                    $"using \"{armour.GetStats(_entries.Values)}\"\n" +
+                    $"data \"RootTemplate\" \"{armour.MapKey}\"\n" +
+                    $"data \"Unique\" \"0\"\n");
+                armourStatNames.Add(statsName);
+            }
+        }
+
+        List<string> generatedTreasureTxt = new() { "new treasuretable \"TUT_Chest_Potions\"\nCanMerge 1" };
+        generatedTreasureTxt.AddRange(armourStatNames.Select(x => $"new subtable \"1,1\"\nobject category \"I_{x}\",1,0,0,0,0,0,0,0"));
+
+        File.WriteAllLines(Path.Combine(destDir, "Armour.txt"), generatedArmourTxt);
+        File.WriteAllLines(Path.Combine(destDir, "TreasureTable.txt"), generatedTreasureTxt);
     }
 }
 
